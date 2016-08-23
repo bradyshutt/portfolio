133e110f01e03bd0676328ea919a4d9d33600a73
Brady <bradyshutt@gmail.com>
2016-08-23 09:41:36 -0700

    Added slide transitions for navigating pages

--- static/js/main.js
+++ static/js/main.js
@@ -3,38 +3,72 @@
 ;(function($) {
 
   $(function pageReady() {
-    $('.arrow-button').click((e) => navTo(e.currentTarget.getAttribute('href')))
-    $('nav a').click((e) => navTo(e.target.getAttribute('href')))
-    
+    $('.arrow-link').click(function(e) {
+      e.preventDefault()
+      navTo(e.currentTarget.getAttribute('href')) 
+    })
+    $('nav li a').click(function(e) {
+      e.preventDefault()
+      if (e.currentTarget === this) {
+        navTo(e.currentTarget.getAttribute('href')) 
+      }
+    })
+    document.body.style.overflow = 'hidden';
   })
 
   let navTo = (function() {
     let pages = ['#page-about', '#page-home', '#page-projects']
-    let curPage = 1
+    let curPageNum = 1
 
-    return function to(page) {
-      let idx
-      if ((idx = pages.indexOf(page)) > -1) {
-        $(pages[curPage]).hide()
-        $(page).show()
+    return function(page) {
+      let newPageNum = pages.indexOf(page)
+      if (newPageNum > -1) {
         $('nav .current').removeClass('current')
-        curPage = idx
-
-      } else if (page === '#next') {
-        $(pages[curPage]).hide()
+        let direction = (newPageNum > curPageNum) ? 'left' : 'right' 
+        swapPages(direction, pages[curPageNum], pages[newPageNum]) 
+        curPageNum = newPageNum
+      }
+      else if (page === '#next') {
         $('nav .current').removeClass('current')
-        curPage = (curPage === pages.length - 1) ? 0 : curPage + 1
-        $(pages[curPage]).show()
-
-      } else if (page === '#prev') {
-        $(pages[curPage]).hide()
+        let newPageNum = (curPageNum === pages.length - 1) ? 0 : curPageNum + 1
+        swapPages('left', pages[curPageNum], pages[newPageNum])
+        curPageNum = newPageNum
+      }
+      else if (page === '#prev') {
+        let newPageNum = (curPageNum === 0) ? pages.length - 1 : curPageNum - 1
         $('nav .current').removeClass('current')
-        curPage = (curPage === 0) ? pages.length - 1 : curPage - 1
-        $(pages[curPage]).show()
+        swapPages('right', pages[curPageNum], pages[newPageNum])
+        curPageNum = newPageNum
       }
 
-      $('nav .button').eq(curPage).addClass('current')
+      $('nav .button').eq(curPageNum).addClass('current')
     }
+
+    function swapPages(dir, oldPage, newPage) {
+      let direction = dir[0].toLowerCase()
+      if (direction !== 'r' && direction !== 'l')
+        throw new Error('Invalid direction to swapPages')
+      let toLeft = (direction === 'l')
+      let pageWidth = $(window).width()
+      let duration = 500
+      $(oldPage)
+        .animate({
+          top: 0,
+          left: toLeft ? (-pageWidth) : pageWidth
+          }, duration )
+      
+      $(newPage)
+        .offset({ 
+          top: 0, 
+          left: toLeft ? pageWidth : (-pageWidth) 
+          })
+        .show()
+        .animate({
+          left: 0, 
+          top: 0 
+          }, duration)
+      }
+
   })()
 
   
